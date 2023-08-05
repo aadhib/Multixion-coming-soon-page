04bb1b3ebc4fa2c035cf7a8769908842b4177808
Aadhib <aadhibnasserv.hussain@gmail.com>
2023-08-05 22:39:59 +0530

    sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://multixion.com',
+  site: 'https://multixion.com', 
   integrations: [tailwind(), sitemap()]
 });
